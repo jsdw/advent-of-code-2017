6689c4cfbe7b546573574a61b08c6729bc605eb9
James Wilson <me@unbui.lt>
2017-12-09 22:05:03 +0000

    redo day07 to be simpler by actually building the tree

--- day07/solution/day07.rs
+++ day07/solution/day07.rs
@@ -5,8 +5,7 @@ use std::env;
 use std::fs;
 use std::io::Read;
 use regex::Regex;
-use std::collections::HashMap;
-use std::cell::Cell;
+use std::collections::{HashMap,HashSet};
 
 // --- Day 7: Recursive Circus ---
 //
@@ -120,89 +119,60 @@ fn main() {
         .read_to_string(&mut content)
         .expect("can't read to string");
 
-    // turn raw input into something structured, ready to work with:
-    let nodes: Vec<Node> = content.lines().filter_map(parse_node).collect();
+    let nodes: HashMap<String,RawNode> = content
+        .lines()
+        .filter_map(parse_raw_node)
+        .map(|node| (node.name.clone(), node))
+        .collect();
 
-    let root_node = star1(&nodes);
+    // Star 1: find the root node of the tree:
+    let root_node: String = find_root(&nodes);
     println!("Star 1: {}", root_node);
 
-    let desired_weight = star2(&nodes, &root_node);
-    println!("Star 2: {}", desired_weight.expect("All weights look ok"));
-
+    // Star 2: build the tree, tracking total weight and balanced state of
+    // each node, then traverse down to the last unabalanced node we find
+    // and find the balanced weight for it:
+    let tree = build_tree(&nodes, &root_node);
+    let mut curr = &tree;
+    while let Some(child) = curr.children.iter().find(|&c| !c.balanced) { curr = child }
+    let correct_weight = find_balanced_weight(&curr.children).unwrap();
+    println!("Star 2: {}", correct_weight);
 }
 
-// create map from child name to parent name. traverse the map from some arbitrary
-// name to find the root parent of the tree.
-fn star1(nodes: &[Node]) -> String {
+// turn a hash of RawNodes into a TreeNode, for easy traversal.
+fn build_tree(nodes: &HashMap<String,RawNode>, root_name: &str) -> TreeNode {
+    let root = &nodes[root_name];
+    let children: Vec<TreeNode> = root.children.iter().map(|c| build_tree(nodes, &**c)).collect();
 
-    let mut child_to_parent: HashMap<&str,&str> = HashMap::new();
-    for node in nodes {
-        for child in &node.children {
-            child_to_parent.insert(child, &node.name);
-        }
+    TreeNode {
+        name: root.name.clone(),
+        weight: root.weight,
+        total_weight: children.iter().map(|c| c.total_weight).sum::<usize>() + root.weight,
+        balanced: children.iter().map(|c| c.balanced).all(|b| b) && find_balanced_weight(&children).is_none(),
+        children: children
     }
-
-    let mut curr: &str = &nodes[0].name;
-    while let Some(next) = child_to_parent.get(curr) {
-        curr = next;
-    }
-    curr.to_owned()
 }
 
-// - work out the total weights and balanced states for each node.
-// - follow the !balanced states down the tree from the root until we get to
-//   a node whose children are balanced. This node is itself unbalanced.
-// - find the differing weights of the children of this unbalanced node,
-//   and return the desired weight of the bad node to rebalance the tree.
-fn star2(nodes: &[Node], root_node: &str) -> Option<isize> {
-
-    let by_name: HashMap<&str,&Node> = nodes.iter().map(|node| (&*node.name,node)).collect();
-    sum_weights(root_node, &by_name);
-
-    let mut curr = by_name[root_node];
-    while let Some(name) = curr.children.iter().find(|&c| !by_name[&**c].balanced.get()) {
-        curr = by_name[&**name];
-    }
-
-    let unbalanced_children: Vec<&Node> = curr.children.iter().map(|c| by_name[&**c]).collect();
-    find_balancing_weight(&unbalanced_children)
-}
-
-// recursively sum up the node weights and balanced statuses.
-// - a nodes total_weight is the weight of it and every node below it.
-// - a node is balanced if its children are all balanced *and* they all
-//   have the same weights.
-fn sum_weights<'a>(name: &str, by_name: &HashMap<&str,&'a Node>) -> &'a Node {
-    let node = &by_name[name];
-    let children: Vec<&Node> = node.children.iter().map(|c| sum_weights(&c, by_name)).collect();
-
-    let mut child_weights: isize = 0;
-    let mut balanced: bool = true;
-    for child in &children {
-        child_weights += child.total_weight.get();
-        if balanced && !child.balanced.get() { balanced = false }
-        else if balanced && find_balancing_weight(&children).is_some() { balanced = false };
-    }
-
-    node.total_weight.set(child_weights + node.weight);
-    node.balanced.set(balanced);
-    node
+// find the root node by finding the node that is not in any children list:
+fn find_root(nodes: &HashMap<String,RawNode>) -> String {
+    let children: HashSet<&str> = nodes.values().flat_map(|n| n.children.iter()).map(|c| &**c).collect();
+    nodes.keys().find(|&name| !children.contains(&**name)).unwrap().to_owned()
 }
 
 // given a list of nodes, find any total weight which differs from the rest
 // and return the weight of the node that would compensate for this difference.
 // if nodes are all the same weight, returns None, as no change is needed.
-fn find_balancing_weight(vals: &[&Node]) -> Option<isize> {
+fn find_balanced_weight(vals: &[TreeNode]) -> Option<usize> {
     let mut it = vals.iter();
     let a = it.next()?;
     let b = it.next()?;
 
-    let a_total = a.total_weight.get();
-    let b_total = b.total_weight.get();
+    let a_total = a.total_weight;
+    let b_total = b.total_weight;
 
     if a_total == b_total {
         for v in it {
-            let v_total = v.total_weight.get();
+            let v_total = v.total_weight;
             if v_total != a_total {
                 return Some(a_total - (v_total - v.weight));
             }
@@ -210,32 +180,41 @@ fn find_balancing_weight(vals: &[&Node]) -> Option<isize> {
         return None;
     }
     if let Some(c) = it.next() {
-        if a_total == c.total_weight.get() {
+        if a_total == c.total_weight {
             return Some(a_total - (b_total - b.weight));
         }
     }
     return Some(b_total - (a_total - a.weight));
 }
 
-// turn a single line of input into a single Node:
-fn parse_node(line: &str) -> Option<Node> {
+// turn a single line of input into a single RawNode:
+fn parse_raw_node(line: &str) -> Option<RawNode> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"^([a-z]+) \(([0-9]+)\)(?: -> ([a-z]+(, [a-z]+)*))?$").unwrap();
     }
     let caps = RE.captures(line)?;
     let name: String          = caps[1].to_owned();
-    let weight: isize         = caps[2].parse().ok()?;
+    let weight: usize         = caps[2].parse().ok()?;
     let children: Vec<String> = caps.get(3).map(|m| m.as_str().split(", ").map(|s| s.to_owned()).collect()).unwrap_or(vec![]);
 
-    Some(Node { name, weight, children, total_weight: Cell::new(0), balanced: Cell::new(true) })
+    Some(RawNode { name, weight, children })
 }
 
-// a single Node. total_weight and balanced are for star 2:
+// a node in tree form, owning its children for each traversal
+// and keeping track of its total weight and balanced state:
 #[derive(Debug,Clone,PartialEq,Eq)]
-struct Node {
+struct TreeNode {
     name: String,
-    weight: isize,
-    children: Vec<String>,
-    total_weight: Cell<isize>,
-    balanced: Cell<bool>
+    weight: usize,
+    children: Vec<TreeNode>,
+    total_weight: usize,
+    balanced: bool
+}
+
+// a single Node:
+#[derive(Debug,Clone,PartialEq,Eq)]
+struct RawNode {
+    name: String,
+    weight: usize,
+    children: Vec<String>
 }
