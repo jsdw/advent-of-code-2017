44b12ccde267abae7a0e4b6399dfe2e62efbcd4d
James Wilson <me@unbui.lt>
2017-12-14 07:38:36 +0000

    day13: tidy up

--- day13/solution/day13.rs
+++ day13/solution/day13.rs
@@ -333,13 +333,13 @@ fn main() {
     let connections: Vec<Layer> = content.lines().filter_map(parse_line).collect();
 
     let severity: usize = connections.iter()
-        .map(|l| step_position(&l, l.depth))
-        .map(|l| if l.position == 0 { l.depth * l.range } else { 0 })
+        .map(|l| (l, step_position(&l, l.depth)))
+        .map(|(l,p)| if p == 0 { l.depth * l.range } else { 0 })
         .sum();
     println!("Star 1: {}", severity);
 
     let delay: usize = (0..)
-        .filter(|&step| connections.iter().map(|l| step_position(&l, l.depth + step)).all(|l| l.position != 0))
+        .filter(|&step| connections.iter().map(|l| step_position(&l, l.depth + step)).all(|l| l != 0))
         .next()
         .unwrap();
     println!("Star 2: {}", delay);
@@ -350,20 +350,13 @@ fn parse_line(line: &str) -> Option<Layer> {
     let mut ns = line.split(": ");
     let depth = ns.next()?.parse().ok()?;
     let range = ns.next()?.parse().ok()?;
-
     Some(Layer{ depth, range, position: 0 })
 }
 
-fn step_position(layer: &Layer, step: usize) -> Layer {
+fn step_position(layer: &Layer, step: usize) -> usize {
     let m = layer.range + if layer.range > 2 { layer.range - 2 } else { 0 };
     let d = (layer.position + step) % m;
-    let pos = if d >= layer.range { m - d } else { d };
-
-    Layer {
-        depth: layer.depth,
-        range: layer.range,
-        position: pos
-    }
+    if d >= layer.range { m - d } else { d }
 }
 
 #[derive(Debug,Clone,Copy)]
