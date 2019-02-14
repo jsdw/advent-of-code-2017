d17fa82cf336e8cd4c8f0d6c3d9b22e70ddbde32
James Wilson <james.wilson@prowler.io>
2019-02-14 08:09:53 +0000

    Improve day02 to use newer rust bits and custom iterator

--- day02/solution/day02.rs
+++ day02/solution/day02.rs
@@ -1,52 +1,46 @@
 use std::env;
-use std::fs;
-use std::io::Read;
 
 fn main() {
 
     let filename = env::args().nth(1).expect("need puzzle input");
 
     // read input to string:
-    let mut content = String::new();
-    fs::File::open(filename)
-        .map_err(|e| format!("{}", e))
-        .expect("can't open file")
-        .read_to_string(&mut content)
-        .expect("can't read to string");
-
-    // Get vector of vectors of u64s for each line:
-    let lines = content
+    let lines = std::fs::read_to_string(filename)
+        .expect("Can't open file")
         .lines()
         .map(|line| line.split_whitespace().filter_map(|s| s.parse().ok()).collect())
         .collect::<Vec<Vec<u64>>>();
 
     // Star 1: diff between max and min for each line:
-    let sum: u64 = lines.iter().map(|l| l.iter().max().unwrap() - l.iter().min().unwrap()).sum();
+    let sum: u64 = lines
+        .iter()
+        .map(|l| l.iter().max().unwrap() - l.iter().min().unwrap())
+        .sum();
     println!("Star 1: {}", sum);
 
     // Star 2: find perfect dividers and sum result for each line:
-    let sum: u64 = lines.iter().map(|l| find_even_divides(&l)).sum();
+    let sum: u64 = lines
+        .iter()
+        .filter_map(|l| find_even_divides(&l))
+        .sum();
     println!("Star 2: {}", sum);
 
 }
 
-fn find_even_divides(input: &[u64]) -> u64 {
-
-    if input.len() == 0 {
-        panic!("Did not find even divides");
+fn find_even_divides(input: &[u64]) -> Option<u64> {
+    for (n1,n2) in tuple_combinations(input) {
+        if n2 % n1 == 0 { return Some(n2 / n1) }
+        else if n1 % n2 == 0 { return Some(n1 / n2) }
     }
+    None
+}
 
-    let first = input[0];
-    for &n in &input[1..] {
-
-        // if we can cleanly divide one number by the other, return the
-        // result of that division..
-        if n >= first && n % first == 0 { return n / first };
-        if n < first && first % n == 0 { return first / n };
-
-    }
-
-    // otherwise, try again with the rest:
-    return find_even_divides(&input[1..]);
-
+// Gives back a reference to each combination of values in the slice provided.
+// We could just use iter-tools to provide a similar thing:
+fn tuple_combinations<'a, T: 'a>(input: &'a [T]) -> impl Iterator<Item=(&T,&T)> + 'a {
+    input.iter().enumerate().flat_map(move |(idx,n1)| {
+        input[idx+1..].iter().map(move |n2| {
+            (n1, n2)
+        })
+    })
 }
